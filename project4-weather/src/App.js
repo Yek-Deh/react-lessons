7b8cdca48fd3add27da3d2421794ede4bc2784bb
Yek-Deh <goran.suliman000@gmail.com>
2025-07-24 14:17:21 +0300

    add-redux-to-project

--- project4-weather/src/App.js
+++ project4-weather/src/App.js
@@ -8,12 +8,16 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Container from "@mui/material/Container";
 import CloudIcon from "@mui/icons-material/Cloud";
 import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 import { useEffect, useState } from "react";
 // EXTERNAL LIBRARIES
 import { useTranslation } from "react-i18next";
-import axios from "axios";
 import moment from "moment/moment";
 import "moment/min/locales";
+// REDUX IMPORT
+import { useSelector, useDispatch } from "react-redux";
+import { fetchWeather } from "./weatherApiSlice";
+
 moment.locale("ar");
 
 const theme = createTheme({
@@ -21,17 +25,19 @@ const theme = createTheme({
     fontFamily: ["IBM"],
   },
 });
-let cancelAxios = null;
 function App() {
+  // REDUX CODE
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => {
+    console.log("============");
+    console.log(state);
+    return state.weather.isLoading;
+  });
+  const temp = useSelector((state) => {
+    return state.weather.weather;
+  });
   const { t, i18n } = useTranslation();
   const [dateAndTime, setDateAndTime] = useState("");
-  const [temp, setTemp] = useState({
-    number: null,
-    description: "",
-    min: null,
-    max: null,
-    icon: null,
-  });
   const [locale, setLocale] = useState("ar");
   const direction = locale == "ar" ? "rtl" : "ltr";
   // ======== EVENT HANDLERS ========= //
@@ -49,47 +55,14 @@ function App() {
     setDateAndTime(moment().format("l"));
   }
   useEffect(() => {
+    // trying redux
+    // dispatch(changeResult());
+    console.log("dispatching fetch weather from the component");
+    dispatch(fetchWeather());
     i18n.changeLanguage(locale);
   }, []);
   useEffect(() => {
     setDateAndTime(moment().format("l"));
-    axios
-      .get(
-        "https://api.openweathermap.org/data/2.5/weather?lat=24.7&lon=46.5&appid=39bfbe9c8845e648c1f1e0f4fee125b3",
-        {
-          cancelToken: new axios.CancelToken((c) => {
-            //clean up for use effect
-            cancelAxios = c;
-          }),
-        }
-      )
-      .then(function (response) {
-        // handle success
-        const responseTemp = Math.round(response.data.main.temp - 272.15);
-        const min = Math.round(response.data.main.temp_min - 272.15);
-        const max = Math.round(response.data.main.temp_max - 272.15);
-        const description = response.data.weather[0].description;
-        const responseIcon = response.data.weather[0].icon;
-
-        setTemp({
-          number: responseTemp,
-          min: min,
-          max: max,
-          description: description,
-          icon: `https://openweathermap.org/img/wn/${responseIcon}@2x.png`,
-        });
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .finally(function () {
-        // always executed
-      });
-    return () => {
-      console.log("canceling");
-      cancelAxios();
-    };
   }, []);
   return (
     <div className="App">
@@ -163,6 +136,11 @@ function App() {
                         alignItems: "center",
                       }}
                     >
+                      {isLoading ? (
+                        <CircularProgress style={{ color: "white" }} />
+                      ) : (
+                        ""
+                      )}
                       <Typography variant="h1" style={{ textAlign: "right" }}>
                         {temp.number}
                       </Typography>
@@ -180,9 +158,13 @@ function App() {
                         alignItems: "center",
                       }}
                     >
-                      <h5>{t("min")}: {temp.min}</h5>
+                      <h5>
+                        {t("min")}: {temp.min}
+                      </h5>
                       <h5 style={{ margin: "0px 5px" }}>|</h5>
-                      <h5>{t("max")}: {temp.max}</h5>
+                      <h5>
+                        {t("max")}: {temp.max}
+                      </h5>
                     </div>
                   </div>
                   {/*== DEGREE & DESCRIPTION ==*/}
